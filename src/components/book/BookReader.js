fa07200e33c5de5b62246717dd7d05d67cec1cff
vilenaNikolaeva <59927349+vilenaNikolaeva@users.noreply.github.com>
2022-01-15 12:56:00 +0200

    update and prepare movie player progress

--- src/components/book/BookReader.js
+++ src/components/book/BookReader.js
@@ -1,55 +1,57 @@
 import react, { useState, useEffect } from "react";
 import styles from "../../assets/scss/componentsStyles/BookReader.module.scss";
 import { useCatalog } from "../../context/CatalogCtx";
-import { useModal } from './../../context/ModalCtx';
-import ModalWrapper from './../../wrappers/ModalWrapper';
+import { useModal } from "./../../context/ModalCtx";
+import ModalWrapper from "./../../wrappers/ModalWrapper";
+import { Document, Page } from "react-pdf/dist/umd/entry.parcel";
 
 const BookReader = () => {
   const { isOpenBookReader, closeModal } = useModal();
   const { book } = useCatalog();
-  // const [numPages, setNumPages] = useState(null);
-  // const [pageNumber, setPageNumber] = useState(1);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
 
-  // const onDocumentLoadSuccess = ({ numPages }) => {
-  //   console.log(numPages);
-  //   setNumPages(numPages);
-  // };
-  // const changePage = (offset) => {
-  //   setPageNumber((prevPageNumber) => prevPageNumber + offset);
-  // };
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
 
-  // const previousPage = () => {
-  //   changePage(-1);
-  // };
+  const changePage = (offset) => {
+    setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  };
 
-  // const nextPage = () => {
-  //   changePage(1);
-  // };
+  const previousPage = () => {
+    changePage(-1);
+  };
+
+  const nextPage = () => {
+    changePage(1);
+  };
 
   return (
     <ModalWrapper
       setIsOpenModalComponent={closeModal}
-      isOpenModalComponent={isOpenBookReader}
-    >
-      <div>
-        {/* <Document
-         file={samplePDF}
-         onLoadSuccess={onDocumentLoadSuccess}>
+      isOpenModalComponent={isOpenBookReader}>
+      <div className={styles.readerContainer}>
+        <Document file={"https://firebasestorage.googleapis.com/v0/b/booksandmovies-cf479.appspot.com/o/books%2FBruder-Grimm_-_Rumpelshtiltshen_-_48620.pdf?alt=media&token=c0ee9ebf-1e0b-4c96-ab5b-4b4c1745e96f"} 
+        onLoadSuccess={onDocumentLoadSuccess}>
           <Page style={styles["modalContent-page"]} pageNumber={pageNumber} />
         </Document>
-        <p>
+        <p className={styles['readerContainer-page']}>
           Page {pageNumber} of {numPages}
         </p>
+        <div>
+        </div>
+        <div className={styles['readerContainer-btnContainer']}>
         <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
           Previous
         </button>
         <button
           type="button"
           disabled={pageNumber >= numPages}
-          onClick={nextPage}
-        >
+          onClick={nextPage}>
           Next
-        </button> */}
+        </button>
+        </div>
       </div>
     </ModalWrapper>
   );
--- src/components/movies/MoviePlayer.js
+++ src/components/movies/MoviePlayer.js
@@ -1,28 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useModal } from "./../../context/ModalCtx";
 import { useCatalog } from "./../../context/CatalogCtx";
 import ModalWrapper from "./../../wrappers/ModalWrapper";
 import ReactPlayer from "react-player";
 
-import styles from '../../assets/scss/componentsStyles/MoviePlayer.module.scss';
+import styles from "../../assets/scss/componentsStyles/MoviePlayer.module.scss";
+import requester from "./../../api/requester";
 
 const MoviePlayer = () => {
   const { isOpenMoviePlayer, closeModal } = useModal();
   const { movie } = useCatalog();
+  const [movieProgress, setMovieProgress] = useState();
+  const [watchComplate, setWatchComplate] = useState(false);
+
+  useEffect(() => {
+    // requester
+  }, [watchComplate]);
+
+  const updatePlayerProgress = (played) => {
+    console.log(played);
+
+    if (played >= 0.9 && !watchComplate) {
+      setWatchComplate(true);
+    } else {
+      //update movie context progress
+
+      
+      //         loaded: 0.8535082819630407
+      // loadedSeconds: 125.46571744856699
+      // played: 0.018559876837386566
+      // playedSeconds: 2.728301895095825
+    }
+  };
   return (
     <ModalWrapper
       setIsOpenModalComponent={closeModal}
       isOpenModalComponent={isOpenMoviePlayer}
     >
       <div className={styles.playerContainer}>
-          <button className={styles['playerContainer-closeBtn']} onClick={closeModal}>X</button>
+        <button
+          className={styles["playerContainer-closeBtn"]}
+          onClick={closeModal}
+        >
+          X
+        </button>
         <ReactPlayer
           url={movie.movieUrl}
           width="100%"
           height="100%"
+          controls={true}
+          // onPlay={movie.progress}
+          onProgress={updatePlayerProgress}
           config={{
             youtube: {
-              playerVars: { origin: "https://www.youtube.com" },
+              playerVars: { origin: "http://localhost:3000" },
             },
             preload: true,
           }}
